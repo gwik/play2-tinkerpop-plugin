65067821515fdadbe0fe6a40c4b832eee741df07
Antonin Amand <antonin.amand@gmail.com>
2012-06-27 03:14:27 +0200

    fix build and README

--- README.md
+++ README.md
@@ -3,9 +3,21 @@
 A GraphDB plugin for [Play 2 framework](http://playframework.org/) using the
 [Tinkerpop blueprints stack](https://github.com/tinkerpop/blueprints/wiki/).
 
-## Instanciate your graph db
+## Installation
+
+### Plugins declaration
+
+Add this to your _conf/play.plugins_ file, create it if it's not there already.
+
+  1000:org.gwikzone.play.tinkerpop.blueprints.orientdb.OrientBlueprintPlugin
+
+### Instanciate your graph db
 
   package utils
 
   import org.gwikzone.play.tinkerpop.blueprints.orientdb.OrientDB
   object GraphDB extends OrientDB
+
+Import GraphDB from this package in your application code. Then if you want to
+use an other graphDB say neo4j you just need to change the import in this
+and you're done.
--- project/Build.scala
+++ project/Build.scala
@@ -1,17 +1,21 @@
 import sbt._
-import Process._
 import Keys._
-
+import java.net.URL
 
 object PluginBuild extends Build {
 
-  val appName = "play2-tinkerpop"
-
-  val appVersion = "1.0-SNAPSHOT"
-
   val typesafeReleases = "Typesafe releases" at "http://repo.typesafe.com/typesafe/releases/"
 
-  lazy val root = Project("plugin", base = file("plugin")).settings(
+  val commonSettings = Seq(
+    organization := "org.gwikzone",
+    version := "1.0-SNAPSHOT",
+    licenses := Seq(("Apache License, Version 2.0",
+        new URL("http://www.apache.org/licenses/LICENSE-2.0.html")))
+  )
+
+  lazy val root = Project("play2-tinkerpop-plugin",
+      base = file("plugin"),
+      settings = Project.defaultSettings ++ commonSettings).settings(
     resolvers += typesafeReleases,
     libraryDependencies ++= Seq (
       "play" %% "play" % "2.0.1",
@@ -20,8 +24,9 @@ object PluginBuild extends Build {
     )
   )
 
-  lazy val orientdbPlugin = Project("orientdb", base = file("impls/orientdb")).
-      settings(
+  lazy val orientdbPlugin = Project("play2-tinkerpop-plugin-orientdb",
+      base = file("impls/orientdb"),
+      settings = Project.defaultSettings ++ commonSettings).settings(
     libraryDependencies +=
       "com.tinkerpop.blueprints" % "blueprints-orient-graph" % "2.0.0"
   ).dependsOn(root)
